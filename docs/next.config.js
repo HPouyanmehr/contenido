b1a6803adfe33fc4102c59e563c009e09e66de83
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-03-20 11:57:22 +0330

    fix: Remove unused packages

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,15 +1,6 @@
-/** @type {import('@next/mdx').withMDX} */
-const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)$/,
-  options: {
-    providerImportSource: '@mdx-js/react',
-  },
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
