58933d7d8166fc5c12840002dba1b1a1648cd706
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-03-21 17:23:25 +0330

    feat: Improve OnThisPage component

--- docs/src/components/common/Sidebar/OnThisPage.tsx
+++ docs/src/components/common/Sidebar/OnThisPage.tsx
@@ -3,10 +3,13 @@ import type { FC } from 'react';
 
 // Custom Core
 import BodyOne from 'components/core/BodyOne';
+import BodyTwo from 'components/core/BodyTwo';
 import Box from 'components/core/Box';
 import AppBarSpacer from 'components/core/AppBarSpacer';
 import Link from 'components/core/Link';
 import { fixHref } from 'utilities/helper/link';
+import Stack from 'components/core/Stack';
+import Divider from 'components/core/Divider';
 
 // Custom Types
 export interface OnThisPageProps {
@@ -33,19 +36,50 @@ const OnThisPage: FC<OnThisPageProps> = (props) => {
         sx={({ spacing }) => ({
           display: 'flex',
           flexDirection: 'column',
+          gap: 2,
           height: '91%',
           p: spacing(2, 1),
         })}
       >
-        <BodyOne gutterBottom>On this page</BodyOne>
-        {headings &&
-          Object.keys(headings).map((headingTwo, index) => (
-            <Box key={headingTwo + index}>
-              <Link href={fixHref(`#${headingTwo}`)} color='text.secondary'>
-                {headingTwo}
-              </Link>
-            </Box>
-          ))}
+        <BodyTwo gutterBottom>On this page</BodyTwo>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1.5 }}>
+          {headings &&
+            Object.keys(headings).map((headingTwo, index) => (
+              <Box
+                key={headingTwo + index}
+                sx={{ display: 'flex', flexDirection: 'column', gap: 0.5 }}
+              >
+                <Link
+                  href={fixHref(`#${headingTwo}`)}
+                  variant='body2'
+                  color='text.secondary'
+                  fontWeight='bold'
+                >
+                  {headingTwo}
+                </Link>
+                {headings[headingTwo].map((headingThree, index) => (
+                  <Link
+                    href={fixHref(`#${headingThree}`)}
+                    variant='body2'
+                    color='text.secondary'
+                    sx={{ marginLeft: 2 }}
+                    key={headingThree + index}
+                  >
+                    {headingThree}
+                  </Link>
+                ))}
+              </Box>
+            ))}
+        </Box>
+        <Divider />
+        <Link
+          href='/'
+          variant='caption'
+          color='text.secondary'
+          sx={{ marginLeft: 2, textTransform: 'none' }}
+        >
+          Edit this page on GitHub &rarr;
+        </Link>
       </Box>
     </Box>
   );
