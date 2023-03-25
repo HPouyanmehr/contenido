5bc2459bbbf6b6d52196b4411fe9a6cba5de7202
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-03-25 18:00:56 +0430

    feat: Config vite for build

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,7 +1,18 @@
-// vite.config.ts
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [react(), tsConfigPaths(), reactRefresh(), dts()],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.tsx'),
+      name: 'Contenido',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `contenido-lib.${format}.js`,
+    },
+  },
 });
