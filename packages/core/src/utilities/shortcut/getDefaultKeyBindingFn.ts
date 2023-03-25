e7bb123e32bebdd933c15b0c2e53aa49b2fbdbcf
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-03-25 16:33:16 +0430

    fix: Remove unused import

--- packages/core/src/utilities/shortcut/getDefaultKeyBindingFn.ts
+++ packages/core/src/utilities/shortcut/getDefaultKeyBindingFn.ts
@@ -1,4 +1,4 @@
-import { getDefaultKeyBinding, KeyBindingUtil, RichUtils } from 'draft-js';
+import { getDefaultKeyBinding, KeyBindingUtil } from 'draft-js';
 
 // Custom Utilities
 import initialShortcuts from './initial';
