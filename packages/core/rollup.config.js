13d7dc504b75dcc574b6ea784297710f46755d3d
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-03-29 22:25:52 +0430

    fix: Fix issues in publishing

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,41 +1,33 @@
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-css-only';
 
-const config = [
-  {
-    input: './src/index.ts',
-    output: [
-      {
-        file: 'dist/index.js',
-        format: 'cjs',
-      },
-      {
-        file: 'dist/index.es.js',
-        format: 'es',
-        exports: 'named',
-      },
-    ],
-    plugins: [
-      postcss({
-        plugins: [],
-        minimize: true,
-      }),
-      typescript(),
-      commonjs(),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
-      }),
-      external(),
-      resolve(),
-      terser(),
-    ],
-  },
-];
+const config = {
+  input: './src/index.ts',
+  output: [
+    {
+      file: './dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  external: ['react', 'react-dom'],
+  plugins: [
+    css(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      clean: true,
+      declaration: true,
+    }),
+  ],
+};
 
 export default config;
