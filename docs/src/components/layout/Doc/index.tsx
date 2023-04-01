ca9d489e220b4c8fb12ba4dc6e970c283429a5ae
Hosein Pouyanmehr <a.h.pouyanmehr@gmail.com>
2023-04-01 16:35:29 +0430

    feat: Add previous and next button to the documentation

--- docs/src/components/layout/Doc/index.tsx
+++ docs/src/components/layout/Doc/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head';
 
 // Types
-import type { FC, ReactNode } from 'react';
+import type { FC } from 'react';
 
 // Custom Core Components
 import AppBarSpacer from 'components/core/AppBarSpacer';
 import Box from 'components/core/Box';
+import ButtonLink from 'components/core/ButtonLink';
+import Divider from 'components/core/Divider';
 
 // Custom Common Components
 import DocHeader from 'components/common/Header/Doc';
@@ -13,10 +15,18 @@ import DocSidebar from 'components/common/Sidebar/Doc';
 import Markdown from 'components/common/Markdown';
 import OnThisPage from 'components/common/Sidebar/OnThisPage';
 
-// Custom Types
+// Custom Section Components
 import DocsFooter from 'components/section/footer/Doc';
 
+// Custom Icon Components
+import ChevronRightIcon from 'components/icon/ChevronRight';
+
+// Custom Utilities
+import { getNextSidebarLink, getPrevSidebarLink } from 'utilities/helper/link';
+
+// Custom Types
 import type { DocProps, DocSidebarProps } from 'types/docs';
+
 export interface PageLayoutProps {
   doc?: DocProps;
   sidebar?: DocSidebarProps[];
@@ -25,6 +35,9 @@ export interface PageLayoutProps {
 const DocumentLayout: FC<PageLayoutProps> = (props) => {
   // Props
   const { doc, sidebar = [] } = props;
+
+  const nextDoc = getNextSidebarLink(sidebar, doc);
+  const prevDoc = getPrevSidebarLink(sidebar, doc);
 
   return (
     <>
@@ -44,12 +57,61 @@ const DocumentLayout: FC<PageLayoutProps> = (props) => {
           <Box
             component='main'
             sx={({ breakpoints }) => ({
+              display: 'flex',
+              flexDirection: 'column',
               flexGrow: 1,
               [breakpoints.down('md')]: { mx: 2 },
             })}
           >
-            <AppBarSpacer />
-            <Markdown>{doc?.content}</Markdown>
+            <Box>
+              <AppBarSpacer />
+              <Markdown>{doc?.content}</Markdown>
+            </Box>
+            {(nextDoc.link || prevDoc.link) && (
+              <>
+                <Divider sx={{ mt: '3rem' }} />
+                <Box
+                  sx={({}) => ({
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    my: '2rem',
+                  })}
+                >
+                  {prevDoc.link ? (
+                    <ButtonLink
+                      href={`/docs${prevDoc.link}`}
+                      sx={{
+                        textTransform: 'capitalize',
+                        color: 'text.primary',
+                      }}
+                      startIcon={
+                        <ChevronRightIcon
+                          sx={{ transform: 'rotate(-180deg)' }}
+                        />
+                      }
+                    >
+                      {prevDoc.title.replaceAll('-', ' ')}
+                    </ButtonLink>
+                  ) : (
+                    <Box />
+                  )}
+                  {nextDoc.link && (
+                    <ButtonLink
+                      href={`/docs${nextDoc.link}`}
+                      sx={{
+                        textTransform: 'capitalize',
+                        color: 'text.primary',
+                      }}
+                      endIcon={<ChevronRightIcon />}
+                    >
+                      {nextDoc.title.replaceAll('-', ' ')}
+                    </ButtonLink>
+                  )}
+                </Box>
+              </>
+            )}
           </Box>
           <Box
             sx={({ breakpoints }) => ({
--- docs/src/utilities/helper/link.ts
+++ docs/src/utilities/helper/link.ts
@@ -1,4 +1,5 @@
 // Types
+import { DocProps, DocSidebarProps } from 'types/docs';
 import type { Url, UrlObject } from 'url';
 
 export const fixHref = (href?: string | URL | Url | UrlObject): string => {
@@ -33,4 +34,83 @@ export const getPurePath = (href?: string): string => {
   if (href.includes('#')) return href.slice(0, href.indexOf('#'));
 
   return href;
+};
+
+export const getNextSidebarLink = (
+  sidebar: DocSidebarProps[],
+  currentValue?: DocProps
+): { link: string; title: string } => {
+  if (sidebar.length > 0 && currentValue) {
+    const sectionIndex = sidebar.findIndex(
+      (section) => section.title === currentValue.section
+    );
+
+    if (sectionIndex > -1) {
+      const section = sidebar[sectionIndex];
+      const itemIndex = section.items.findIndex(
+        (item) => item.link === currentValue.id
+      );
+
+      if (itemIndex > -1) {
+        if (itemIndex + 1 < section.items.length) {
+          const nextItem = section.items[itemIndex + 1];
+          return {
+            link: `/${section.title}/${nextItem.link}`,
+            title: nextItem.title,
+          };
+        } else if (sectionIndex + 1 < sidebar.length) {
+          const nextSection = sidebar[sectionIndex + 1];
+
+          if (nextSection.items.length > 0) {
+            return {
+              link: `/${nextSection.title}/${nextSection.items[0].link}`,
+              title: nextSection.items[0].title,
+            };
+          }
+        }
+      }
+    }
+  }
+
+  return { link: '', title: '' };
+};
+
+export const getPrevSidebarLink = (
+  sidebar: DocSidebarProps[],
+  currentValue?: DocProps
+): { link: string; title: string } => {
+  if (sidebar.length > 0 && currentValue) {
+    const sectionIndex = sidebar.findIndex(
+      (section) => section.title === currentValue.section
+    );
+
+    if (sectionIndex > -1) {
+      const section = sidebar[sectionIndex];
+      const itemIndex = section.items.findIndex(
+        (item) => item.link === currentValue.id
+      );
+
+      if (itemIndex > -1) {
+        if (itemIndex !== 0) {
+          const prevItem = section.items[itemIndex - 1];
+          return {
+            link: `/${section.title}/${prevItem.link}`,
+            title: prevItem.title,
+          };
+        } else if (sectionIndex !== 0) {
+          const prevSection = sidebar[sectionIndex - 1];
+
+          if (prevSection.items.length > 0) {
+            const lastItemIndex = prevSection.items.length - 1;
+            return {
+              link: `/${prevSection.title}/${prevSection.items[lastItemIndex].link}`,
+              title: prevSection.items[lastItemIndex].title,
+            };
+          }
+        }
+      }
+    }
+  }
+
+  return { link: '', title: '' };
 };
